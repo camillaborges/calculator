014fe1e7c5745e554909d707ef342f2ea6ea8008
Borges Camilla <camillaborgeshelena@gmail.com>
2021-09-28 17:04:04 +0200

    implementing improvements

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let historic = [];
 function select(sum, subtraction, multiplication, division) {
 
    function sum(fieldOne, fieldTwo) {
-      return parseInt(fieldOne) + parseInt(fieldTwo);
+      return parseFloat(parseInt(fieldOne) + parseInt(fieldTwo));
    }
 
    function subtraction(fieldOne, fieldTwo) {
@@ -22,27 +22,55 @@ function select(sum, subtraction, multiplication, division) {
    const fieldOne = document.querySelector('.field-one').value;
    const fieldTwo = document.querySelector('.field-two').value;
    let result;
-   const operation = fieldOperator.value;
+   const operations = fieldOperator.value;
+   const operationSimble = fieldOperator
+  
 
-   switch (operation) {
-    case '+':  
-      result = sum(fieldOne, fieldTwo);
-    break;
-    case '-':
-      result = subtraction(fieldOne,fieldTwo);
-    break;
-    case '*':
-      result = multiplication(fieldOne, fieldTwo);
-    break;
-    case '/':
-      result = division(fieldOne, fieldTwo);  
+   function getOperation(type) {
+      let operation = {
+         'sum': function() {
+            return result = sum(fieldOne,fieldTwo)
+         },
+         'subtraction': function(){
+            return result = subtraction(fieldOne,fieldTwo);
+         },
+         'division': function(){
+            return result = division(fieldOne, fieldTwo);
+         },
+         'multiplication': function(){
+            return result =  multiplication(fieldOne, fieldTwo)
+         } 
+      };
+      return operation[type]();
    }
 
-   document.querySelector('.result').innerHTML = result;
+   let operator = getOperation(operations)
+   
+
+
+   
+   
+   
+   
+   /* switch (operation) {
+    case 'sum':  
+      result = sum(fieldOne, fieldTwo);
+    break;
+    case 'subtraction':
+      result = subtraction(fieldOne,fieldTwo);
+    break;
+    case 'multiplication':
+      result = multiplication(fieldOne, fieldTwo);
+    break;
+    case 'division':
+      result = division(fieldOne, fieldTwo);  
+   } */
+
+   document.querySelector('.result').innerHTML = Math.round(result*100)/100;
    document.querySelector('.form-calculator').reset();   
    
 
-   let item = {fieldOne, fieldTwo, result, operation};
+   let item = {fieldOne, fieldTwo, result,operator, operationSimble};
    historic.push(item);
 
    insertHistoryItem(item);
@@ -50,13 +78,13 @@ function select(sum, subtraction, multiplication, division) {
 
 function insertHistoryItem(item) {
   let div = document.createElement('div'); // is a node
-  div.innerHTML = `<div class='item'> ${item.fieldOne} ${item.operation} ${item.fieldTwo} = ${item.result}</div>`;
+  div.innerHTML = `<div class='item'> ${item.fieldOne} ${item.operationSimble.innerHTML} ${item.fieldTwo} = ${Math.round(item.operator *100)/100}</div>`;
   document.querySelector('.historical-result').appendChild(div);
 }
 
 function deleteHistory() {
  document.querySelector('.result').innerHTML = '';
  document.querySelector('.historical-result').innerHTML = '';
- 
+   
  
 }
